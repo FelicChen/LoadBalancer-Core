bc2d3668085ae55504da820569bb123d3191a048
FelicChen <kuma1203@outlook.com>
2023-02-20 12:29:45 +0800

    update

--- LBS/Program.cs
+++ LBS/Program.cs
@@ -2,26 +2,31 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 
-var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
-var builder = WebHost
-    .CreateDefaultBuilder(args)
-    .UseConfiguration(config)
-    .UseStartup<Startup>()
-    .UseKestrel(options =>
-    {
-        var listens = config.GetSection("ServerSettings:Listens").Get<List<ListenModel>>();
-        foreach (var listen in listens)
+const string appUnid = "{78e2a518-bf77-4773-9590-aa89ed811dd8}";
+using (Mutex m = new(false, $"Global\\{appUnid}"))
+{
+    if (!m.WaitOne(0, false)) return;
+    var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+    var builder = WebHost
+        .CreateDefaultBuilder(args)
+        .UseConfiguration(config)
+        .UseStartup<Startup>()
+        .UseKestrel(options =>
         {
-            if (listen.Https)
+            var listens = config.GetSection("ServerSettings:Listens").Get<List<ListenModel>>();
+            foreach (var listen in listens)
             {
-                options.ListenAnyIP(listen.Port, o => o.UseHttps());
+                if (listen.Https)
+                {
+                    options.ListenAnyIP(listen.Port, o => o.UseHttps());
+                }
+                else
+                {
+                    options.ListenAnyIP(listen.Port);
+                }
             }
-            else
-            {
-                options.ListenAnyIP(listen.Port);
-            }
-        }
-    });
+        });
 
-var app = builder.Build();
-app.Run();
+    var app = builder.Build();
+    app.Run();
+}
